dc3488d8cdd5216da0225ad9f6730fcb6502dedc
nsauret <nsauret@f1r6s12.paris.42.school>
2026-04-17 17:28:57 +0200

    Added chat; Added send button; Better resizing

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ var selected_tool = "pen"
 var selected_thickness = "medium"
 
 
+
+
 function set_select_color(color)
 {
 	document.getElementById("SelectedColor").style.backgroundColor = color;
@@ -62,74 +64,108 @@ class Drawing_board extends React.Component
 
 	render() {
 		return (
-			<div className="Background">
+			<div>
+				<div className="FilmGrain"></div>
 
-				<div className="Board">
+				<div className="Background">
 
-				</div>
+					<div className="UpperPart">
 
-				<div className="Foot">
+						<div className="Board">
 
-					<div className="ColorsEnsemble">
-						<div id="SelectedColor" className="SelectedColor" style={{ backgroundColor: selected_color }}></div>
-						<div className="ColorsButtonsBackground">
-							<button onClick={() => set_select_color("#ffffff")} className={`${"ColorButton"} ${"White"}`}></button>
-							<button onClick={() => set_select_color("#D9D9D9")} className={`${"ColorButton"} ${"LightGrey"}`}></button>
-							<button onClick={() => set_select_color("#FF0000")} className={`${"ColorButton"} ${"LightRed"}`}></button>
-							<button onClick={() => set_select_color("#FF6A00")} className={`${"ColorButton"} ${"LightOrange"}`}></button>
-							<button onClick={() => set_select_color("#FFC300")} className={`${"ColorButton"} ${"LightYellow"}`}></button>
-							<button onClick={() => set_select_color("#95FF00")} className={`${"ColorButton"} ${"LightGreen"}`}></button>
-							<button onClick={() => set_select_color("#00D9FF")} className={`${"ColorButton"} ${"LightSkyBlue"}`}></button>
-							<button onClick={() => set_select_color("#0033FF")} className={`${"ColorButton"} ${"LightOceanBlue"}`}></button>
-							<button onClick={() => set_select_color("#A100FF")} className={`${"ColorButton"} ${"LightPurple"}`}></button>
-							<button onClick={() => set_select_color("#F200FF")} className={`${"ColorButton"} ${"LightPink"}`}></button>
-							<button onClick={() => set_select_color("#BC4F51")} className={`${"ColorButton"} ${"LightBrown"}`}></button>
-
-							<button onClick={() => set_select_color("#000000")} className={`${"ColorButton"} ${"Black"}`}></button>
-							<button onClick={() => set_select_color("#666666")} className={`${"ColorButton"} ${"DarkGrey"}`}></button>
-							<button onClick={() => set_select_color("#930000")} className={`${"ColorButton"} ${"DarkRed"}`}></button>
-							<button onClick={() => set_select_color("#983F00")} className={`${"ColorButton"} ${"DarkOrange"}`}></button>
-							<button onClick={() => set_select_color("#977400")} className={`${"ColorButton"} ${"DarkYellow"}`}></button>
-							<button onClick={() => set_select_color("#548F00")} className={`${"ColorButton"} ${"DarkGreen"}`}></button>
-							<button onClick={() => set_select_color("#0094AE")} className={`${"ColorButton"} ${"DarkSkyBlue"}`}></button>
-							<button onClick={() => set_select_color("#001876")} className={`${"ColorButton"} ${"DarkOceanBlue"}`}></button>
-							<button onClick={() => set_select_color("#6800A4")} className={`${"ColorButton"} ${"DarkPurple"}`}></button>
-							<button onClick={() => set_select_color("#95009D")} className={`${"ColorButton"} ${"DarkPink"}`}></button>
-							<button onClick={() => set_select_color("#772E30")} className={`${"ColorButton"} ${"DarkBrown"}`}></button>
-						</div>
-					</div>
-
-					<div className="ToolsEnsemble">
-						<div className="Tools">
-							<button onClick={() => set_select_tool("pen")} className={`${"ToolButton"} ${"Pen"}`}>
-							<svg xmlns="http://www.w3.org/2000/svg" height="100%" viewBox="0 -960 960 960" width="100%" fill="#000000"><path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/></svg>
-							</button>
-							<button onClick={() => set_select_tool("eraser")} className={`${"ToolButton"} ${"Eraser"}`}>
-								<svg xmlns="http://www.w3.org/2000/svg" height="100%" viewBox="0 -960 960 960" width="100%" fill="#000000"><path d="M690-240h190v80H610l80-80Zm-500 80-85-85q-23-23-23.5-57t22.5-58l440-456q23-24 56.5-24t56.5 23l199 199q23 23 23 57t-23 57L520-160H190Zm296-80 314-322-198-198-442 456 64 64h262Zm-6-240Z"/></svg>
-							</button>
-							<button onClick={() => set_select_tool("bucket")} className={`${"ToolButton"} ${"Bucket"}`}>
-								<svg xmlns="http://www.w3.org/2000/svg" height="100%" viewBox="0 -960 960 960" width="100%" fill="#000000"><path d="M346-140 100-386q-10-10-15-22t-5-25q0-13 5-25t15-22l230-229-106-106 62-65 400 400q10 10 14.5 22t4.5 25q0 13-4.5 25T686-386L440-140q-10 10-22 15t-25 5q-13 0-25-5t-22-15Zm47-506L179-432h428L393-646Zm399 526q-36 0-61-25.5T706-208q0-27 13.5-51t30.5-47l42-54 44 54q16 23 30 47t14 51q0 37-26 62.5T792-120Z"/></svg>
-							</button>
 						</div>
 
-						<div className="Thickness">
-							<button onClick={() => set_select_thickness("thin")} className={`${"ToolButton"} ${"Fine_thickness"}`}>
-								<svg xmlns="http://www.w3.org/2000/svg" height="100%" viewBox="0 -960 960 960" width="100%" fill="#000000"><path d="M280-200q-33 0-56.5-23.5T200-280q0-15 6-29.5t18-26.5l400-400q12-12 26.5-18t29.5-6q33 0 56.5 23.5T760-680q0 15-5.5 30T737-623L337-223q-12 12-26.5 17.5T280-200Z"/></svg>
-							</button>
-							<button onClick={() => set_select_thickness("medium")} className={`${"ToolButton"} ${"Medium_thickness"}`}>
-								<svg xmlns="http://www.w3.org/2000/svg" height="100%" viewBox="0 -960 960 960" width="100%" fill="#000000"><path d="M340-200q-58 0-99-41t-41-99q0-27 10.5-53t30.5-46l280-280q20-20 46-30.5t53-10.5q58 0 99 41t41 99q0 27-10.5 53T719-521L439-241q-20 20-46 30.5T340-200Z"/></svg>
-							</button>
-							<button onClick={() => set_select_thickness("thick")} className={`${"ToolButton"} ${"Thick_thickness"}`}>
-								<svg xmlns="http://www.w3.org/2000/svg" height="100%" viewBox="0 -960 960 960" width="100%" fill="#000000"><path d="M402-120q-118 0-200-82t-82-200q0-54 20-105.5t62-93.5l157-157q42-42 93.5-62T558-840q118 0 200 82t82 200q0 54-20 105.5T758-359L601-202q-42 42-93.5 62T402-120Z"/></svg>
-							</button>
+						<div className="ChatBox">
+							<div className="Chat">
+								<b>Paul:</b> c moche <br/>
+								<b>Pauline:</b> oui <br/>
+								<b>Zoe:</b> d'accord <br/>
+								<b>Nathan:</b> pas gentil ;( <br/>
+								<b>Nathan:</b> oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
+							</div>
+
+							<div className="ChatInputBox">
+								<input className="ChatInput"></input>
+
+								<button className="SendChatButton">
+									<svg xmlns="http://www.w3.org/2000/svg" height="100%" viewBox="0 -960 960 960" width="100%" fill="#000000"><path d="M120-160v-640l760 320-760 320Zm80-120 474-200-474-200v140l240 60-240 60v140Zm0 0v-400 400Z"/></svg>
+								</button>
+							</div>
 						</div>
+						
+						
 
 					</div>
 
-					<button onClick={clear_board} className={`${"ToolButton"} ${"Trash"}`}>
-						<svg xmlns="http://www.w3.org/2000/svg" height="100%" viewBox="0 -960 960 960" width="100%" fill="#000000"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
-					</button>
+					<div className="Foot">
+
+						<div className="ColorsEnsemble">
+							<div id="SelectedColor" className="SelectedColor" style={{ backgroundColor: selected_color }}></div>
+							<div className="ColorsButtonsBackground">
+								<button onClick={() => set_select_color("#ffffff")} className={`${"ColorButton"} ${"White"}`}></button>
+								<button onClick={() => set_select_color("#D9D9D9")} className={`${"ColorButton"} ${"LightGrey"}`}></button>
+								<button onClick={() => set_select_color("#FF0000")} className={`${"ColorButton"} ${"LightRed"}`}></button>
+								<button onClick={() => set_select_color("#FF6A00")} className={`${"ColorButton"} ${"LightOrange"}`}></button>
+								<button onClick={() => set_select_color("#FFC300")} className={`${"ColorButton"} ${"LightYellow"}`}></button>
+								<button onClick={() => set_select_color("#95FF00")} className={`${"ColorButton"} ${"LightGreen"}`}></button>
+								<button onClick={() => set_select_color("#00D9FF")} className={`${"ColorButton"} ${"LightSkyBlue"}`}></button>
+								<button onClick={() => set_select_color("#0033FF")} className={`${"ColorButton"} ${"LightOceanBlue"}`}></button>
+								<button onClick={() => set_select_color("#A100FF")} className={`${"ColorButton"} ${"LightPurple"}`}></button>
+								<button onClick={() => set_select_color("#F200FF")} className={`${"ColorButton"} ${"LightPink"}`}></button>
+								<button onClick={() => set_select_color("#BC4F51")} className={`${"ColorButton"} ${"LightBrown"}`}></button>
+
+								<button onClick={() => set_select_color("#000000")} className={`${"ColorButton"} ${"Black"}`}></button>
+								<button onClick={() => set_select_color("#666666")} className={`${"ColorButton"} ${"DarkGrey"}`}></button>
+								<button onClick={() => set_select_color("#930000")} className={`${"ColorButton"} ${"DarkRed"}`}></button>
+								<button onClick={() => set_select_color("#983F00")} className={`${"ColorButton"} ${"DarkOrange"}`}></button>
+								<button onClick={() => set_select_color("#977400")} className={`${"ColorButton"} ${"DarkYellow"}`}></button>
+								<button onClick={() => set_select_color("#548F00")} className={`${"ColorButton"} ${"DarkGreen"}`}></button>
+								<button onClick={() => set_select_color("#0094AE")} className={`${"ColorButton"} ${"DarkSkyBlue"}`}></button>
+								<button onClick={() => set_select_color("#001876")} className={`${"ColorButton"} ${"DarkOceanBlue"}`}></button>
+								<button onClick={() => set_select_color("#6800A4")} className={`${"ColorButton"} ${"DarkPurple"}`}></button>
+								<button onClick={() => set_select_color("#95009D")} className={`${"ColorButton"} ${"DarkPink"}`}></button>
+								<button onClick={() => set_select_color("#772E30")} className={`${"ColorButton"} ${"DarkBrown"}`}></button>
+							</div>
+						</div>
+
+						<div className="ToolsEnsemble">
+							<div className="Tools">
+								<button onClick={() => set_select_tool("pen")} className={`${"ToolButton"} ${"Pen"}`}>
+								<svg xmlns="http://www.w3.org/2000/svg" height="100%" viewBox="0 -960 960 960" width="100%" fill="#000000"><path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/></svg>
+								</button>
+								<button onClick={() => set_select_tool("eraser")} className={`${"ToolButton"} ${"Eraser"}`}>
+									<svg xmlns="http://www.w3.org/2000/svg" height="100%" viewBox="0 -960 960 960" width="100%" fill="#000000"><path d="M690-240h190v80H610l80-80Zm-500 80-85-85q-23-23-23.5-57t22.5-58l440-456q23-24 56.5-24t56.5 23l199 199q23 23 23 57t-23 57L520-160H190Zm296-80 314-322-198-198-442 456 64 64h262Zm-6-240Z"/></svg>
+								</button>
+								<button onClick={() => set_select_tool("bucket")} className={`${"ToolButton"} ${"Bucket"}`}>
+									<svg xmlns="http://www.w3.org/2000/svg" height="100%" viewBox="0 -960 960 960" width="100%" fill="#000000"><path d="M346-140 100-386q-10-10-15-22t-5-25q0-13 5-25t15-22l230-229-106-106 62-65 400 400q10 10 14.5 22t4.5 25q0 13-4.5 25T686-386L440-140q-10 10-22 15t-25 5q-13 0-25-5t-22-15Zm47-506L179-432h428L393-646Zm399 526q-36 0-61-25.5T706-208q0-27 13.5-51t30.5-47l42-54 44 54q16 23 30 47t14 51q0 37-26 62.5T792-120Z"/></svg>
+								</button>
+							</div>
+							&nbsp;&nbsp;
+							<div className="Thickness">
+								<button onClick={() => set_select_thickness("thin")} className={`${"ToolButton"} ${"Fine_thickness"}`}>
+									<svg xmlns="http://www.w3.org/2000/svg" height="100%" viewBox="0 -960 960 960" width="100%" fill="#000000"><path d="M280-200q-33 0-56.5-23.5T200-280q0-15 6-29.5t18-26.5l400-400q12-12 26.5-18t29.5-6q33 0 56.5 23.5T760-680q0 15-5.5 30T737-623L337-223q-12 12-26.5 17.5T280-200Z"/></svg>
+								</button>
+								<button onClick={() => set_select_thickness("medium")} className={`${"ToolButton"} ${"Medium_thickness"}`}>
+									<svg xmlns="http://www.w3.org/2000/svg" height="100%" viewBox="0 -960 960 960" width="100%" fill="#000000"><path d="M340-200q-58 0-99-41t-41-99q0-27 10.5-53t30.5-46l280-280q20-20 46-30.5t53-10.5q58 0 99 41t41 99q0 27-10.5 53T719-521L439-241q-20 20-46 30.5T340-200Z"/></svg>
+								</button>
+								<button onClick={() => set_select_thickness("thick")} className={`${"ToolButton"} ${"Thick_thickness"}`}>
+									<svg xmlns="http://www.w3.org/2000/svg" height="100%" viewBox="0 -960 960 960" width="100%" fill="#000000"><path d="M402-120q-118 0-200-82t-82-200q0-54 20-105.5t62-93.5l157-157q42-42 93.5-62T558-840q118 0 200 82t82 200q0 54-20 105.5T758-359L601-202q-42 42-93.5 62T402-120Z"/></svg>
+								</button>
+								&nbsp;&nbsp;
+								<button onClick={clear_board} className={`${"ToolButton"} ${"Trash"}`}>
+									<svg xmlns="http://www.w3.org/2000/svg" height="100%" viewBox="0 -960 960 960" width="100%" fill="#000000"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
+								</button>
+							</div>
+
+						</div>
+
+						<button className="SendDrawingButton">
+							SEND
+						</button>
+						
+					</div>
 				</div>
+
 			</div>
 		);
 	}
