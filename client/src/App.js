cce44cc042d62eb8745ef52ed29eaef89d4a0d37
Nathan Sauret <nsauret@f2r2s12.clusters.42paris.fr>
2026-04-22 15:27:49 +0200

    save

--- client/src/App.js
+++ client/src/App.js
@@ -142,6 +142,18 @@ function export_drawing()
 	}, 2000);
 }
 
+function send_message_enter_key(event, ele) {
+	if(event.key === 'Enter') {
+		alert(ele.value);
+	}
+}
+function send_message()
+{
+	var message = document.getElementById("ChatInput").value;
+	document.getElementById("ChatInput").value = "";
+	console.log("Message sent in chat:", message);
+}
+
 
 
 const Board = () => {
@@ -331,9 +343,9 @@ class DrawingInterface extends React.Component
 								</div>
 
 								<div className="ChatInputBox">
-									<input placeholder='Chat here...' id="ChatInput" className="ChatInput"></input>
+									<input onKeyDown={send_message_enter_key} placeholder='Chat here...' id="ChatInput" className="ChatInput"></input>
 
-									<button className="SendChatButton">
+									<button onClick={send_message} className="SendChatButton">
 										<svg xmlns="http://www.w3.org/2000/svg" height="100%" viewBox="0 -960 960 960" width="100%" fill="#000000"><path d="M120-160v-640l760 320-760 320Zm80-120 474-200-474-200v140l240 60-240 60v140Zm0 0v-400 400Z"/></svg>
 									</button>
 								</div>
