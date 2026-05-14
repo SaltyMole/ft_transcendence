713051265516fb0697d281b93423d5731e9a4100
nsauret <nsauret@f6r3s7.paris.42.school>
2026-05-14 15:28:17 +0200

    Started lobby

--- src/pages/Lobby.jsx
+++ src/pages/Lobby.jsx
@@ -1,18 +1,19 @@
 import test from "../img/test.jpeg"
 import React from 'react';
 import '../css/Lobby.css';
+import "../css/front/style.css";
 
-function Loading()
+function Lobby()
 {
 	return (
-	<>
-		<main class= "bg-cover bg-center h-screen" style={{ backgroundImage: `url(${test})` }}>
-			<div >
-				<h1> LOBBY </h1>
-			</div>
-		</main>
-	</>
-	)
+		<>
+			<main class="bg-cover bg-center h-screen" style={{ backgroundImage: `url(${test})` }}>
+				<div className="overlay">
+					<h1> LOBBY </h1>
+				</div>
+			</main>
+		</>
+	);
 }
 
-export default Loading;
+export default Lobby;
