4c25fd1c428b076943311cb5345fa3ef1a9b0ead
Nathan Sauret <sauret.nathan.pro@gamil.com>
2026-05-19 16:06:40 +0200

    save

---
+++ src/game/getPlayerByName.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from "react";
+
+const getPlayers = async ( gameID ) => {
+	const response = await fetch('/src/game/bdd.json');
+	const data = await response.json();
+	const game = data.find(game => game.id === gameID);
+
+	if (!game) {
+		throw new Error(`Game ${gameID} not found`);
+	}
+
+	return game.players.{};
+};
+
+export default getPlayers;
