c9c0c53376046702584e21cb1ca12072574115ca
zmurie <zmurie@f6r3s5.paris.42.school>
2026-06-08 11:19:36 +0200

    modif totp

--- api/src/utils/totp.ts
+++ api/src/utils/totp.ts
@@ -1,19 +1,29 @@
 import { generateSecret, generate, verify, generateURI } from "otplib";
+import QRCode from 'qrcode'
+
+
 
 // Generate a secret
-const secret = generateSecret();
+export const generateTotpSecret = (): string => generateSecret()
 
-// Generate a TOTP token
-const token = await generate({ secret });
 
-// Verify a token — returns VerifyResult, not a boolean
-const result = await verify({ secret, token });
-console.log(result.valid); // true or false
+
+export const generateQrCode = async (otpauthUrl: string): Promise<string> =>
+  QRCode.toDataURL(otpauthUrl)
 
 // Generate QR code URI for authenticator apps
 
-const uri = (email: string, secret: string): string => generateURI({
+export const generateOtpauthUrl = (email: string, secret: string): string => generateURI({
   issuer: "ft_transcendence",
   label: email,
   secret,
 });
+
+export const verifyTotpCode = (token: string, secret: string): boolean => {
+  try {
+      const result =  verify({ secret, token });
+    return (true)
+  } catch {
+    return false
+  }
+}
