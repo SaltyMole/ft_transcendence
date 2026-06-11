36e18b2ccbbc6ea45b8eafc702a2fdf2d7187797
nsauret <nsauret@f6r2s6.paris.42.school>
2026-06-11 14:36:44 +0200

    Now removing a player removes his drawing

--- api/src/controllers/gameController.ts
+++ api/src/controllers/gameController.ts
@@ -111,6 +111,15 @@ export const removePlayer = async (req: AuthenticatedRequest, res: Response) => 
       return res.status(400).json({ error: 'Player not in this game' })
     }
 
+	// Delete his weapon
+	await db.delete(weaponDrawings).where(
+  		and(eq(weaponDrawings.gameId, gameId), eq(weaponDrawings.userId, playerId))
+	)
+	await db.delete(gamePlayers).where(
+		and(eq(gamePlayers.gameId, gameId), eq(gamePlayers.userId, playerId))
+	)
+
+	// Delete the player
 	await db.delete(gamePlayers).where(
 		and(eq(gamePlayers.gameId, gameId), eq(gamePlayers.userId, playerId))
 	)
